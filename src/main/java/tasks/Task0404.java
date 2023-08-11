8c763f108fa7c1b7ecfd352433ec1b3908131138
Dmitriy Korolev <59607937+KaramazovBrothers@users.noreply.github.com>
2023-08-11 17:14:57 +0200

    Resolve task 0404

---
+++ src/main/java/tasks/Task0404.java
@@ -0,0 +1,13 @@
+package tasks;
+
+public class Task0404 {
+        private static int catsCount = 0;
+
+        public static void addNewCat() {
+            Task0404.catsCount = Task0404.catsCount + 1;
+        }
+
+        public static void main(String[] args) {
+
+        }
+}
