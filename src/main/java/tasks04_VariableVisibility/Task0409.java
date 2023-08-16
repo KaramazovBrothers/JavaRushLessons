8bc60d9daf7e892e43d0d7dd2ad72f2bc462b420
Dmitriy Korolev <59607937+KaramazovBrothers@users.noreply.github.com>
2023-08-16 15:32:46 +0200

    Task 0409 intervals with if else

---
+++ src/main/java/tasks04_VariableVisibility/Task0409.java
@@ -0,0 +1,33 @@
+package tasks04_VariableVisibility;
+
+public class Task0409 {
+    public static void main(String[] args) {
+        checkInterval(60);
+        checkInterval(112);
+        checkInterval(10);
+        checkInterval2(99);
+        checkInterval2(100);
+        checkInterval2(50);
+    }
+
+    public static void checkInterval(int a) {
+        if (a > 50 && a < 100) {
+            System.out.println("Число " + a + " содержится в интервале.");
+        } else {
+            System.out.println("Число " + a + " не содержится в интервале.");
+        }
+    }
+
+    public static void checkInterval2(int a) {
+        if (a < 100) {
+            if (a > 50) {
+                System.out.println("Число " + a + " содержится в интервале.");
+            } else {
+                System.out.println("Число " + a + " не содержится в интервале.");
+            }
+        } else {
+            System.out.println("Число " + a + " не содержится в интервале.");
+        }
+    }
+}
+
