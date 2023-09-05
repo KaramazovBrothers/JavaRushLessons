71735179f05bba6844f13ad3752c3b9a2141a9bc
Dmitriy Korolev <59607937+KaramazovBrothers@users.noreply.github.com>
2023-09-05 22:21:56 +0200

    Tasks 0427_28_29 Determine if a number is positive or negative

---
+++ src/main/java/tasks04_VariableVisibility/Task0429.java
@@ -0,0 +1,23 @@
+package tasks04_VariableVisibility;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Task0429 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int f = 0;
+        int g = 0;
+
+        for (int i = 0; i < 3; i++) {
+            int a = Integer.parseInt(reader.readLine());
+            if (a > 0) {
+                f++;
+            } else if (a < 0) {
+                g++;
+            }
+        }
+        System.out.println("количество отрицательных чисел: " + g + "\n" + "количество положительных чисел: " + f);
+    }
+}
