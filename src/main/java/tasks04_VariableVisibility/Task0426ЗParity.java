8cdca2c2dcaf6f038a35d0a0feaab9f7f52714b5
Dmitriy Korolev <59607937+KaramazovBrothers@users.noreply.github.com>
2023-09-05 21:17:21 +0200

    Task0426 Parity

---
+++ src/main/java/tasks04_VariableVisibility/Task0426ЗParity.java
@@ -0,0 +1,23 @@
+package tasks04_VariableVisibility;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Task0426ЗParity {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(reader.readLine());
+
+        if (a < 0 && a % 2 == 0){
+            System.out.println("отрицательное четное число");
+        } else if (a < 0 && a % 2 == -1) {
+            System.out.println("отрицательное нечетное число");
+        } else if (a == 0) {
+            System.out.println("ноль");
+        } else if (a > 0 && a % 2 == 0) {
+            System.out.println("положительное четное число");
+        } else
+            System.out.println("положительное нечетное число");
+    }
+}
