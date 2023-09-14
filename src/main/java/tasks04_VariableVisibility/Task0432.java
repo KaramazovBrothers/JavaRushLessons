add13b46c814ae1a6788ae64805b3eeead93f66b
Dmitriy Korolev <59607937+KaramazovBrothers@users.noreply.github.com>
2023-09-14 16:23:39 +0200

    Tasks about While

---
+++ src/main/java/tasks04_VariableVisibility/Task0432.java
@@ -0,0 +1,17 @@
+package tasks04_VariableVisibility;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class Task0432 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        int a = Integer.parseInt(reader.readLine());
+
+        for (int i = 1; i <= a; i++) {
+            System.out.println(s);
+        }
+    }
+}
